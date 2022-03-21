e62aa8ff99b1a1d978fa8631f18f0b49d15502ba
Carlos Moragon <cmoragoncorella@gmail.com>
2022-03-21 18:40:42 +0100

    la interfaz funciona

--- modelosDeComputacion/lenguaje1/src/interfaz/Interfaz.java
+++ modelosDeComputacion/lenguaje1/src/interfaz/Interfaz.java
@@ -1,33 +1,44 @@
 package interfaz;
 import dominio.*;
+import java.util.List;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.function.*;
 import java.util.stream.*;
+import java.util.stream.Stream;
 public class Interfaz{
 	private Scanner sc = new Scanner(System.in);
-	private ArrayList<String> lista = new ArrayList<>();
+
+	private List<String> lista = new ArrayList<>();
+
 	private String lenguaje = sc.next();
+
 	LenguajeAB generarAB = new LenguajeAB();
 	Lenguaje01 generar01 = new Lenguaje01();
-	Stream.Builder<String> stBuilder = Stream.Builder();
-	private Supplier<Stream<String>> generar = () -> {
-		Stream<String> solucion = Stream<>();
+
+	Stream.Builder<String> stBuilder = Stream.builder(); 
+
+	private Consumer<List<String>> generar = x -> {
+//		Stream<String> solucion =  stBuilder.add("").build();
 		if(lenguaje.equalsIgnoreCase("ab")){
 			for(int i = 0; i < 20; i++){
 				int n = (int) Math.random() * 10;
-				solucion += stBuilder.add(generarAB.generar(n, "")).build();
+				x.add(generarAB.generar(n, ""));
+			}
 			//return Stream.iterate(generarAB.generar(n,"")).limit(20);
 		}else if(lenguaje.equalsIgnoreCase("01")){
-			solucion += stBuilder.add(generar01.generar("")).build();
+			for(int i = 0; i< 20; i++){
+				x.add(generar01.generar(""));
 			//return Stream.iterate(generar01.generar("")).limit(20);
+			}
 		}
-		return solucion;
+		//return solucion;
 	};
 	
-	public ArrayList<String> procesarPeticion(){
+	public List<String> procesarPeticion(){
 		if(lenguaje.equalsIgnoreCase("01") || lenguaje.equalsIgnoreCase("ab")){
-			lista = generar.get().collect(Collectors.toList());
+			generar.accept(lista);
+			//lista = generar.get().collect(Collectors.toList());
 		}else{
 			System.out.println("introduce: AB o 01");
 		}
