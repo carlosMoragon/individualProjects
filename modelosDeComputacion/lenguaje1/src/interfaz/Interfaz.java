ece24ac0ccf8731580320e564c09bb19269c3126
Carlos Moragon <cmoragoncorella@gmail.com>
2022-03-21 17:45:48 +0100

    reenfocado lenguaje 1, mediante un Stream.Builder()

--- modelosDeComputacion/lenguaje1/src/interfaz/Interfaz.java
+++ modelosDeComputacion/lenguaje1/src/interfaz/Interfaz.java
@@ -10,13 +10,19 @@ public class Interfaz{
 	private String lenguaje = sc.next();
 	LenguajeAB generarAB = new LenguajeAB();
 	Lenguaje01 generar01 = new Lenguaje01();
+	Stream.Builder<String> stBuilder = Stream.Builder();
 	private Supplier<Stream<String>> generar = () -> {
+		Stream<String> solucion = Stream<>();
 		if(lenguaje.equalsIgnoreCase("ab")){
-			int n = (int) Math.random() * 10;
-			return Stream.iterate(generarAB.generar(n,"")).limit(20);
+			for(int i = 0; i < 20; i++){
+				int n = (int) Math.random() * 10;
+				solucion += stBuilder.add(generarAB.generar(n, "")).build();
+			//return Stream.iterate(generarAB.generar(n,"")).limit(20);
 		}else if(lenguaje.equalsIgnoreCase("01")){
-			return Stream.iterate(generar01.generar("")).limit(20);
+			solucion += stBuilder.add(generar01.generar("")).build();
+			//return Stream.iterate(generar01.generar("")).limit(20);
 		}
+		return solucion;
 	};
 	
 	public ArrayList<String> procesarPeticion(){
