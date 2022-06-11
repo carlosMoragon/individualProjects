296ecdb2e83fb964f58fd81196b2116e5db4bb5b
Carlos Moragon <cmoragoncorella@gmail.com>
2022-06-12 00:51:17 +0200

    minotaurio

---
+++ Minotaruo/src/aplicacion/Principal.java
@@ -0,0 +1,12 @@
+package aplicacion;
+
+import dominio.*;
+import interfaz.Interfaz;
+
+public class Principal{
+	public static void main(String args[]){
+		Interfaz llamada = new Interfaz();
+		llamada.procesarLlamada();
+
+	}
+}
